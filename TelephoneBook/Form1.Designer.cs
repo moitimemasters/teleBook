49a20148cf8b9bdffa7a1a083b1d9b175124226d
Иван Песня <ivanpesnia04@gmail.com>
2020-04-30 18:47:32 +0300

    still working...

---
+++ TelephoneBook/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TelephoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name_add_edit = new System.Windows.Forms.Label();
+            this.name_ae_box = new System.Windows.Forms.TextBox();
+            this.number_ae_box = new System.Windows.Forms.TextBox();
+            this.name_s_box = new System.Windows.Forms.TextBox();
+            this.number_s_box = new System.Windows.Forms.TextBox();
+            this.number_add_edit = new System.Windows.Forms.Label();
+            this.name_search = new System.Windows.Forms.Label();
+            this.number_search = new System.Windows.Forms.Label();
+            this.add_button = new System.Windows.Forms.Button();
+            this.edit_button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(627, 250);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // name_add_edit
+            // 
+            this.name_add_edit.AutoSize = true;
+            this.name_add_edit.Location = new System.Drawing.Point(25, 27);
+            this.name_add_edit.Name = "name_add_edit";
+            this.name_add_edit.Size = new System.Drawing.Size(29, 13);
+            this.name_add_edit.TabIndex = 1;
+            this.name_add_edit.Text = "Имя";
+            // 
+            // name_ae_box
+            // 
+            this.name_ae_box.Location = new System.Drawing.Point(81, 27);
+            this.name_ae_box.Name = "name_ae_box";
+            this.name_ae_box.Size = new System.Drawing.Size(174, 20);
+            this.name_ae_box.TabIndex = 2;
+            // 
+            // number_ae_box
+            // 
+            this.number_ae_box.Location = new System.Drawing.Point(81, 58);
+            this.number_ae_box.Name = "number_ae_box";
+            this.number_ae_box.Size = new System.Drawing.Size(108, 20);
+            this.number_ae_box.TabIndex = 3;
+            // 
+            // name_s_box
+            // 
+            this.name_s_box.Location = new System.Drawing.Point(367, 27);
+            this.name_s_box.Name = "name_s_box";
+            this.name_s_box.Size = new System.Drawing.Size(181, 20);
+            this.name_s_box.TabIndex = 4;
+            this.name_s_box.TextChanged += new System.EventHandler(this.name_s_box_TextChanged);
+            // 
+            // number_s_box
+            // 
+            this.number_s_box.Location = new System.Drawing.Point(367, 58);
+            this.number_s_box.Name = "number_s_box";
+            this.number_s_box.Size = new System.Drawing.Size(115, 20);
+            this.number_s_box.TabIndex = 5;
+            this.number_s_box.TextChanged += new System.EventHandler(this.number_s_box_TextChanged);
+            // 
+            // number_add_edit
+            // 
+            this.number_add_edit.AutoSize = true;
+            this.number_add_edit.Location = new System.Drawing.Point(25, 61);
+            this.number_add_edit.Name = "number_add_edit";
+            this.number_add_edit.Size = new System.Drawing.Size(41, 13);
+            this.number_add_edit.TabIndex = 6;
+            this.number_add_edit.Text = "Номер";
+            // 
+            // name_search
+            // 
+            this.name_search.AutoSize = true;
+            this.name_search.Location = new System.Drawing.Point(261, 30);
+            this.name_search.Name = "name_search";
+            this.name_search.Size = new System.Drawing.Size(92, 13);
+            this.name_search.TabIndex = 7;
+            this.name_search.Text = "Поиск по имени:";
+            // 
+            // number_search
+            // 
+            this.number_search.AutoSize = true;
+            this.number_search.Location = new System.Drawing.Point(261, 61);
+            this.number_search.Name = "number_search";
+            this.number_search.Size = new System.Drawing.Size(100, 13);
+            this.number_search.TabIndex = 8;
+            this.number_search.Text = "Поиск по номеру: ";
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(25, 111);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(75, 23);
+            this.add_button.TabIndex = 9;
+            this.add_button.Text = "Добавить";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // edit_button
+            // 
+            this.edit_button.Location = new System.Drawing.Point(106, 111);
+            this.edit_button.Name = "edit_button";
+            this.edit_button.Size = new System.Drawing.Size(75, 23);
+            this.edit_button.TabIndex = 10;
+            this.edit_button.Text = "Изменить";
+            this.edit_button.UseVisualStyleBackColor = true;
+            this.edit_button.Click += new System.EventHandler(this.edit_button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.edit_button);
+            this.Controls.Add(this.add_button);
+            this.Controls.Add(this.number_search);
+            this.Controls.Add(this.name_search);
+            this.Controls.Add(this.number_add_edit);
+            this.Controls.Add(this.number_s_box);
+            this.Controls.Add(this.name_s_box);
+            this.Controls.Add(this.number_ae_box);
+            this.Controls.Add(this.name_ae_box);
+            this.Controls.Add(this.name_add_edit);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Телефонная Книга";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label name_add_edit;
+        private System.Windows.Forms.TextBox name_ae_box;
+        private System.Windows.Forms.TextBox number_ae_box;
+        private System.Windows.Forms.TextBox name_s_box;
+        private System.Windows.Forms.TextBox number_s_box;
+        private System.Windows.Forms.Label number_add_edit;
+        private System.Windows.Forms.Label name_search;
+        private System.Windows.Forms.Label number_search;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.Button edit_button;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TelephoneBook/Form1.cs
@@ -0,0 +1,269 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            select_all();
+        }
+        public void select_all()
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `contacts`" , db.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        public bool is_name_in_table(String name)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `contacts` WHERE `name` = '" + name + "'", db.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table.Rows.Count > 0;
+
+        }
+
+        public bool is_number_in_table(String number)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `contacts` WHERE `phone` = '" + number + "'", db.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table.Rows.Count > 0;
+
+        }
+
+        public void select_by_name_and_number(String name, String number)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `contacts` WHERE (`name` LIKE '%" + name + "%') AND (`phone` LIKE '%" + number + "%')", db.getConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        public void delete_by_name(String name)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("DELETE FROM `contacts` WHERE `name` = '" + name + "'", db.getConnection());
+            adapter.DeleteCommand = command;
+            command.ExecuteNonQuery();
+            db.closeConnection();
+        }
+        public void delete_by_number(String number)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("DELETE FROM `contacts` WHERE `phone` = '" + number + "'", db.getConnection());
+            adapter.DeleteCommand = command;
+            command.ExecuteNonQuery();
+            db.closeConnection();
+
+        }
+        public void delete_by_id(String id)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("DELETE FROM `contacts` WHERE `id` = '" + id + "'", db.getConnection());
+            adapter.DeleteCommand = command;
+            command.ExecuteNonQuery();
+            db.closeConnection();
+        }
+
+        public void update_name_by_number(String new_name, String number)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("UPDATE `contacts` SET `name` = '" + new_name + "' WHERE `phone` = '" + number + "'", db.getConnection());
+            db.closeConnection();
+        }
+        public void update_name_by_id(String new_name, String id)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("UPDATE  `contacts` SET `name` = '" + new_name + "' WHERE `id` = '" + id + "'", db.getConnection());
+            command.ExecuteNonQuery();
+            db.closeConnection();
+        }
+        public void update_number_by_name(String new_number, String name)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlConnection conn = db.getConnection();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("UPDATE  `contacts` SET `phone` = '" + new_number + "' WHERE `name` = '" + name + "'", conn);
+            command.ExecuteNonQuery();
+            db.closeConnection();
+            
+        }
+        public void update_number_by_id(String new_number, String id)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlConnection conn = db.getConnection();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("UPDATE  `contacts` SET `phone` = '" + new_number + "' WHERE `id` = '" + id + "'", conn);
+            command.ExecuteNonQuery();
+            db.closeConnection();
+        }
+        public void insert_name_number(String name, String number)
+        {
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `contacts` (`name`, `phone`) VALUES ('" + name + "', '" + number + "')", db.getConnection());
+            adapter.InsertCommand = command;
+            try
+            {
+                command.ExecuteNonQuery();
+            } catch
+            {
+                MessageBox.Show("Номер уже записан в книге.");
+            }
+            db.closeConnection();
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            String name = name_ae_box.Text;
+            String number = number_ae_box.Text;
+            insert_name_number(name, number);
+            select_all();
+        }
+
+        private void edit_button_Click(object sender, EventArgs e)
+        {
+            String name = name_ae_box.Text;
+            String number = number_ae_box.Text;
+            bool is_name = is_name_in_table(name);
+            bool is_number = is_number_in_table(number);
+            if (is_name && is_number)
+            {
+                MessageBox.Show("Запись с именем " + name + " и номером " + number + " есть в книге.");
+            } else if (is_name && !is_number)
+            {
+                update_number_by_name(number, name);
+            } else if (!is_name && is_number)
+            {
+                update_name_by_number(name, number);
+            } else
+            {
+                MessageBox.Show("Записи не существует.");
+            }
+            select_all();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String name = name_ae_box.Text;
+            String number = number_ae_box.Text;
+            bool is_name = is_name_in_table(name);
+            bool is_number = is_number_in_table(number);
+            if (name != "" && number != "") {
+                if (!is_name && is_number)
+                {
+                    MessageBox.Show("Такого имени нет, чтобы удалить запись по номеру не вводите имя.");
+                } else if (!is_number && is_name)
+                {
+                    MessageBox.Show("Такого номера нет, чтобы удалить записи по имени не вводите номер.");
+                } else if (!is_number && !is_name)
+                {
+                    MessageBox.Show("Таких данных нет, введите валидные данные.");
+                } else
+                {
+                    delete_by_number(number);
+                }
+            } else if (name != "" && number == "")
+            {
+                if (!is_name)
+                {
+                    MessageBox.Show("Такого имени нет.");
+                } else
+                {
+                    delete_by_name(name);
+                }
+            }
+            else if (number != "" && name == "")
+            {
+                if (!is_number)
+                {
+                    MessageBox.Show("Такого номера нет.");
+                }
+                else
+                {
+                    delete_by_number(number);
+                }
+            } else
+            {
+                MessageBox.Show("Введите что-нибудь!");
+            }
+            select_all();
+        }
+
+        private void name_s_box_TextChanged(object sender, EventArgs e)
+        {
+            String name = name_s_box.Text;
+            String number = number_s_box.Text;
+            select_by_name_and_number(name, number);
+        }
+
+        private void number_s_box_TextChanged(object sender, EventArgs e)
+        {
+            String name = name_s_box.Text;
+            String number = number_s_box.Text;
+            select_by_name_and_number(name, number);
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int row_index = e.RowIndex;
+            int col_index = e.ColumnIndex;
+            String id = dataGridView1.Rows[row_index].Cells[0].Value.ToString();
+            String name = dataGridView1.Rows[row_index].Cells[1].Value.ToString();
+            String number = dataGridView1.Rows[row_index].Cells[2].Value.ToString();
+            if (col_index == 1)
+            {
+                update_name_by_id(name, id);
+            } else if (col_index == 2)
+            {
+                update_number_by_id(number, id);
+            }
+        }
+    }
+}
